6c37d9a3f5ce197621075ca1859d0cc4f7ccccf7
Hariharan Ravichandran <hariharanr1799@gmail.com>
2025-01-27 13:29:57 -0500

    Modified arm launch file to use hebi_arm param for backward compatibility

--- launch/arm.launch.py
+++ launch/arm.launch.py
@@ -1,89 +1,87 @@
-import os
-import yaml
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration, LogInfo, OpaqueFunction
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch.actions import DeclareLaunchArgument, SetLaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.conditions import IfCondition
-from ament_index_python.packages import get_package_share_directory  # Correct import
-
-# Helper OpaqueFunction for extracting the arm's name
-def setup_hebi_arm(context, *args, **kwargs):
-    config_package = LaunchConfiguration('config_package').perform(context)
-    config_file = LaunchConfiguration('config_file').perform(context)
-    
-    # Resolve the full path to the config file
-    config_file_path = os.path.join(get_package_share_directory(config_package), 'config', config_file)
-
-    # Load the YAML config file and extract the model name
-    with open(config_file_path, 'r') as file:
-        config_data = yaml.safe_load(file)
-        hrdf_path = config_data['hrdf']
-        hebi_arm = os.path.basename(hrdf_path).split('.')[0]  # Extract the model name (e.g., A-2085-06)
-
-    return [SetLaunchConfiguration('hebi_arm', hebi_arm)]
+from launch.conditions import IfCondition, LaunchConfigurationEquals
 
 
 def generate_launch_description():
 
     # Declare arguments
-    declared_arguments = [
-        
-        # Config files
+    declared_arguments = []
+    declared_arguments.append(
         DeclareLaunchArgument(
-            "config_file",
-            default_value="None",
-            description="Config file for the arm of type `.cfg.yaml`",
-        ),
+            "hebi_arm",
+            description="Name of the robot to be used.",
+        )
+    )
+    declared_arguments.append(
         DeclareLaunchArgument(
-            "config_package",
-            default_value="hebi_description",  
-            description="Package of the arm's config file. Usually the argument is not set, \
-                        it enables use of a custom config.",
-        ),
-
-        # Description
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="hebi_description",  
-            description="Description package of the urdf file. Usually the argument is not set, \
-                        it enables use of a custom description.",
-        ),
-
-        # RViz stuff
+            "prefix",
+            default_value="",
+            description="Prefix for the HEBI Arm name. Usually the argument is not set",
+        )
+    )
+    declared_arguments.append(
         DeclareLaunchArgument(
             "use_rviz",
             default_value="true",
             description="Whether to start RViz.",
-        ),
-
-        # Prefix
+        )
+    )
+    declared_arguments.append(
         DeclareLaunchArgument(
-            "prefix",
-            default_value="",
-            description="Prefix for the arm. Usually the argument is not set",
-        ),
-    ]
+            "description_package",
+            default_value="hebi_description",
+            description="Description package of the URDF. Usually the argument is not set, \
+                        it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "config_package",
+            default_value="hebi_description",
+            description="Package of the arm's config file. Usually the argument is not set, \
+                        it enables use of a custom config.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "config_file",
+            default_value="None",  # Default set later
+            description="Config file for the arm of type `.cfg.yaml`",
+        )
+    )
+    declared_arguments.append(
+        SetLaunchConfiguration(
+            name="config_file",
+            value=PythonExpression(['"', LaunchConfiguration("hebi_arm"), '.cfg.yaml"']),
+            condition=LaunchConfigurationEquals("config_file", "None"),
+        )
+    )
 
+    hebi_arm = LaunchConfiguration("hebi_arm")
     prefix = LaunchConfiguration("prefix")
     description_package = LaunchConfiguration("description_package")
+    config_package = LaunchConfiguration("config_package")
+    config_file = LaunchConfiguration("config_file")
+    use_rviz = LaunchConfiguration("use_rviz")
 
-    # Use OpaqueFunction to set up the hebi_arm variable dynamically
-    setup_hebi_arm_action = OpaqueFunction(function=setup_hebi_arm)
-
-    # Correct concatenation using PathJoinSubstitution and TextSubstitution
+    # Get URDF via xacro
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare(description_package), "urdf", "kits"]),
-            "/",
-            LaunchConfiguration('hebi_arm'),
-            ".urdf.xacro ",
-            " ",
-            "prefix:=", # TODO: needs to be fixed/standardized
+            PathJoinSubstitution(
+                [
+                    FindPackageShare(description_package),
+                    "urdf",
+                    "kits",
+                    PythonExpression(['"', hebi_arm, '.urdf.xacro"']),
+                ]
+            ),
+            " " "prefix:=",
             prefix,
         ]
     )
@@ -93,44 +91,40 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[
-            {"robot_description": robot_description_content}
-        ],
+        parameters=[{"robot_description": robot_description_content}],
         namespace=prefix,
     )
 
     # Define the arm node
     arm_node = Node(
-        package='hebi_ros2_examples',
-        executable='arm_node',
-        name='arm_node',
-        output='screen',
+        package="hebi_ros2_examples",
+        executable="arm_node",
+        name="arm_node",
+        output="screen",
         parameters=[
-            {'config_package': LaunchConfiguration('config_package')},
-            {'config_file': LaunchConfiguration('config_file')}
+            {"config_package": config_package},
+            {"config_file": config_file},
+            {"prefix": prefix},
         ],
         namespace=prefix,
     )
-    
+
     # Load RViz
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "hebi_arm.rviz"]
-    )
+    rviz_config_file = PathJoinSubstitution([FindPackageShare(description_package), "rviz", "hebi_arm.rviz"])
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
         arguments=["-d", rviz_config_file],
-        condition=IfCondition(LaunchConfiguration("use_rviz")),
+        condition=IfCondition(use_rviz),
     )
 
     return LaunchDescription(
-        declared_arguments +
-        [
-            setup_hebi_arm_action,  # Execute the setup action to set hebi_arm
+        declared_arguments
+        + [
             robot_state_publisher_node,
             arm_node,
-            rviz_node
+            rviz_node,
         ]
     )
